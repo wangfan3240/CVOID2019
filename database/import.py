e4812c72d4da955149563f129f1dcfd75296ba78
wangfan3240 <wf.3240@163.com>
2020-12-16 15:44:43 +0800

    修改了地图

--- database/import.py
+++ database/import.py
@@ -1,6 +1,6 @@
 import pandas
 import csv, sqlite3
 conn= sqlite3.connect("dbname.db")
-df = pandas.read_csv('D:/CVOID/database/gdp.csv')
-df.to_sql('gdp', conn, if_exists='append', index=False)
+df = pandas.read_csv('D:/CVOID/database/Overall.csv')
+df.to_sql('CVOID', conn, if_exists='append', index=False)
 print('ok')
